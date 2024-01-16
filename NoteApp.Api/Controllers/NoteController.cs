bbb5e0476371e30c27a3f3c706e1874d25f77cac
gokhan <mertgokhandonmez@gmail.com>
2024-01-16 18:50:45 +0300

    User and Note controllers filled

--- NoteApp.Api/Controllers/NoteController.cs
+++ NoteApp.Api/Controllers/NoteController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NoteApp.Business.Concrete;
 using NoteApp.DataAccess;
+using NoteApp.Entities.Concrete;
 
 namespace NoteApp.Api.Controllers;
 
@@ -16,20 +17,70 @@ public class NoteController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public Task<IActionResult> Get(int id)
+    public async Task<IActionResult> GetNoteById(int id)
     {
         try
         {
-            // var result = noteManager.
-            // if (true)
-            // {
-                
-            // }
-            return null;
+            var result = noteManager.GetById(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else { return NotFound("No note was found with this id.");}
+            
         }
-        catch (System.Exception)
+        catch (System.Exception ex)
         {
+            return BadRequest(ex.Message);
+            throw;
+        }
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateNote([FromBody] Note note)
+    {
+        try
+        {
+            noteManager.Insert(note);
+            return Ok();
+        }
+        catch (System.Exception ex)
+        {
+            return BadRequest(ex.Message);
+            throw;
+        }
+    }
 
+    [HttpDelete]
+    public async Task<IActionResult> DeleteNote([FromBody] Note note)
+    {
+        try
+        {
+            noteManager.Delete(note);
+            return Ok($"Successfully deleted note with this id: {note.NoteId}");
+        }
+        catch (System.Exception ex)
+        {
+            return BadRequest(ex.Message);
+            throw;
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAllNotesByUserId(int id)
+    {
+        try
+        {
+            var result = noteManager.GetAllNotesByUserId(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else { return NotFound("No note was found with this id!");}
+        }
+        catch (System.Exception ex)
+        {
+            return BadRequest(ex.Message);
             throw;
         }
     }
