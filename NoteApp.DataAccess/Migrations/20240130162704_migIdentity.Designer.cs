10b701bc1e597e8a9e42fae9930dd92983d8c658
gokhan <mertgokhandonmez@gmail.com>
2024-02-01 18:39:30 +0300

    identity appRole

--- NoteApp.DataAccess/Migrations/20240130162704_migIdentity.Designer.cs
+++ NoteApp.DataAccess/Migrations/20240130162704_migIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteApp.DataAccess;
 
@@ -11,9 +12,11 @@ using NoteApp.DataAccess;
 namespace NoteApp.DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240130162704_migIdentity")]
+    partial class migIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
